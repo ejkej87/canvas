db2e366e58198fb13e94d24eded3a9083b079a0f
Arkadiusz Garzel <ejkej87@gmail.com>
2022-10-26 08:56:28 +0200

    add canvas

---
+++ main.js
@@ -0,0 +1,71 @@
+const colorPicker = document.querySelector('#colorPicker')
+const brushWidth = document.querySelector('#brushWidth')
+const container = document.querySelector('.container')
+const clear = document.querySelector('#clear')
+let canvas = document.createElement("canvas")
+
+canvas.style.border = "2px solid #000"
+document.body.appendChild(canvas)
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let context = canvas.getContext("2d")
+
+context.lineCap = "round"
+
+let isMouseDown = false
+let previous = {
+    x: 0,
+    y: 0
+}
+const buttons = document.querySelectorAll('.color_picker button')
+
+for (btn of buttons) {
+    btn.addEventListener('click', (e) => {
+        console.log(e.currentTarget.dataset.color)
+        context.strokeStyle = e.currentTarget.dataset.color
+    })
+}
+
+canvas.addEventListener("mousemove", event => {
+    if (isMouseDown) {
+
+        let {
+            pageX: x,
+            pageY: y
+        } = event
+        context.beginPath()
+        context.moveTo(previous.x, previous.y)
+        context.lineWidth = `${brushWidth.value}`
+        context.lineTo(x, y)
+        context.stroke()
+
+        previous = {
+            x,
+            y
+        }
+    }
+})
+
+canvas.addEventListener("mousedown", event => {
+    console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`);
+    let {
+        pageX: x,
+        pageY: y
+    } = event
+    previous = {
+        x,
+        y
+    }
+
+    isMouseDown = true
+})
+
+canvas.addEventListener("mouseup", () => {
+    isMouseDown = false
+})
+
+clear.addEventListener('click', () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+})
