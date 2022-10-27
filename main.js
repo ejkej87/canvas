902442fdc18f0b80eb8d476a51caeaea0a65bbb8
Arkadiusz Garzel <ejkej87@gmail.com>
2022-10-27 09:33:21 +0200

    add rect to canvas

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const brushWidth = document.querySelector('#brushWidth')
 const container = document.querySelector('.container')
 const clearButton = document.querySelector('#clear')
 const canvas = document.querySelector("#canvas")
+const rect = canvas.getBoundingClientRect()
+const buttons = document.querySelectorAll('.color_picker button')
+console.log(rect)
 
 // canvas.width = window.innerWidth 
 // canvas.height = window.innerHeight
@@ -16,53 +19,52 @@ let previous = {
     x: 0,
     y: 0
 }
-const buttons = document.querySelectorAll('.color_picker button')
 
-for (btn of buttons) {
-    btn.addEventListener('click', (e) => {
-        console.log(e.currentTarget.dataset.color)
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
         context.strokeStyle = e.currentTarget.dataset.color
     })
+}, false)
+
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
 }
 
 canvas.addEventListener("mousemove", event => {
     if (isMouseDown) {
-
         let {
             pageX: x,
             pageY: y
         } = event
         context.beginPath()
         context.moveTo(previous.x, previous.y)
-        context.lineWidth = `${brushWidth.value}`
+        context.lineWidth = brushWidth.value.toString()
         context.lineTo(x, y)
         context.stroke()
 
         previous = {
-            x,
-            y
+            x: event.pageX,
+            y: event.pageY
         }
     }
-})
+}, false)
 
 canvas.addEventListener("mousedown", event => {
     console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`);
-    let {
-        pageX: x,
-        pageY: y
-    } = event
     previous = {
-        x,
-        y
+        x: event.pageX,
+        y: event.pageY
     }
-
     isMouseDown = true
-})
+}, false)
 
 canvas.addEventListener("mouseup", () => {
     isMouseDown = false
-})
+}, false)
 
 clearButton.addEventListener('click', () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
-})
+}, false)
